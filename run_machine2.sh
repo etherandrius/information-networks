1d00b0ce0fc9c28f00a28fdff1a4069bd7aa192e
etherandrius <etherandrius@gmail.com>
2019-01-26 13:53:57 +0000

    removed useless files, fixed script

--- run_machine2.sh
+++ run_machine2.sh
@@ -1,13 +1,14 @@
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.05 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.85 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins,KDE,LNN_1,LNN_2,KSG" -bs=512 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1" -af=relu
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1" -af=sigmoid
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1" -af=linear
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=32 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=64 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=1024 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,8,6,4,2,1"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,10,10,8,2"
-python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="12,8,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.05 -mie="bins" -bs=512 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.85 -mie="bins" -bs=512 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins,KDE,LNN_1,LNN_2,KSG" -bs=512 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,8,6,4" -af=relu
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,8,6,4" -af=sigmoid
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,8,6,4" -af=linear
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=32 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=64 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=1024 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,8,6,4"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="10,10"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="Dr,10,Dr,8,Dr,6,Dr,4,Dr"
+python3.6 main_mi.py -e=1500 -s=15 -ts=0.45 -mie="bins" -bs=512 -ns="8,4"
