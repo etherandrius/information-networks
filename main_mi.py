cfdd08009a47e2c3505dfd6b55502884b3dea8ed
etherandrius <etherandrius@gmail.com>
2019-01-28 15:39:25 +0000

    data_set -> processor

--- main_mi.py
+++ main_mi.py
@@ -7,9 +7,9 @@ from data.data import get_information_processor
 def main():
     params = parameters()
 
-    data_set = get_information_processor(params)
-    (x_train, y_train), (x_test, y_test) = (data_set.x_train, data_set.y_train), (data_set.x_test, data_set.y_test)
-    categories = data_set.categories
+    processor = get_information_processor(params)
+    (x_train, y_train), (x_test, y_test) = (processor.x_train, processor.y_train), (processor.x_test, processor.y_test)
+    categories = processor.categories
     model = networks.get_model_categorical(
         input_shape=x_train[0].shape, network_shape=params.shape, categories=categories, activation=params.activation)
 
@@ -17,7 +17,7 @@ def main():
     batch_size = params.batch_size if params.batch_size > 0 else len(x_train)
     no_of_batches = (len(x_train) / batch_size) * params.epochs
     information_callback = CalculateInformationCallback(
-        model, data_set.information_calculator, data_set.x_full, params.skip, params.cores, no_of_batches)
+        model, processor.information_calculator, processor.x_full, params.skip, params.cores, no_of_batches)
     model.fit(x_train, y_train,
               batch_size=batch_size,
               callbacks=[information_callback],
@@ -27,9 +27,9 @@ def main():
 
     append = ",b-" + str(information_callback.batch)
     print("Saving data to files")
-    data_set.save(append=append)
+    processor.save(append=append)
     print("Producing image")
-    data_set.plot(append=append)
+    processor.plot(append=append)
     print("Done")
     return
 
