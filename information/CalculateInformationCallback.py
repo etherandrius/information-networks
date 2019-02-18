9013dde4a36e91f7e5c64689640af5840df81a94
etherandrius <etherandrius@gmail.com>
2019-02-18 10:31:26 +0000

    made progress bar optional.

--- information/CalculateInformationCallback.py
+++ information/CalculateInformationCallback.py
@@ -4,24 +4,26 @@ from tqdm import tqdm
 
 
 class CalculateInformationCallback(keras.callbacks.Callback):
-
-    def __init__(self, model, information_processor, x_test, no_of_batches):
+    def __init__(self, model, information_processor, x_test, no_of_batches, progress_bar=True):
         super().__init__()
         outputs = [layer.output for layer in model.layers]
         self.batch = 0
         self.__functor = K.function([model.input, K.learning_phase()], outputs)
         self.__x_test = x_test
         self.__ip = information_processor
-        self.__progress = tqdm(total=no_of_batches)
+        self.__progress_bar = progress_bar
+        if progress_bar:
+            self.__progress = tqdm(total=no_of_batches)
 
     def on_batch_end(self, batch, logs=None):
-        self.__progress.update(1)
+        if self.__progress_bar:
+            self.__progress.update(1)
         self.batch += 1
-        if self.batch % 16 == 0:
-            out = self.__functor([self.__x_test, 0.])
-            self.__ip.calculate_information(out, self.batch)
+        out = self.__functor([self.__x_test, 0.])
+        self.__ip.calculate_information(out, self.batch)
 
     def on_train_end(self, logs=None):
         self.__ip.finish_information_calculation()
-        self.__progress.close()
+        if self.__progress_bar:
+            self.__progress.close()
 
